f771a95a9e2d84b78b2671c8063e7a99e4169439
smarkericmark <52010110+smarkericmark@users.noreply.github.com>
2022-01-20 12:11:33 -0500

    Add files via upload

--- CupandBall.py
+++ CupandBall.py
@@ -1,10 +1,15 @@
+#Written in gedit
+#tested on Kali 64x Linux Distro using Python 3.9.9
+
 from random import shuffle
+
+#Create and randomize list
 
 myList = ['empty','empty','RedBall']
 shuffle(myList)
 
-#print(myList)
 
+#Get the players guess
 
 def playerGuess():
 	
@@ -13,10 +18,11 @@ def playerGuess():
 	while guess not in ['1','2','3']:	
 		guess = input('Find the red ball! Pick any of these three cups!!! 1, 2, or 3 ')
 		
-	return int(guess)
-
+	return int(guess) -1 #Adding -1 to align with pick since lists start at zero
 
 guess = playerGuess()
+
+#Compare the guess to the players choice and determine if they've won
 
 def checkGuess(mixedUpList,guess):
 	if mixedUpList[guess] == 'RedBall':
@@ -25,4 +31,4 @@ def checkGuess(mixedUpList,guess):
 		print("Wrong Cup!")
 		print(myList) 
 
-checkGuess(myList, guess-1)	
+checkGuess(myList, guess)	
